8cbceb2dd78d6995dbf1051c35b0f2504bb2e9bc
Júlia Romero <juliarore6@gmail.com>
2025-11-23 18:27:26 +0100

    feat: add ArticleItem component to app.ts imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArticleItem} from './components/article-item/article-item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArticleItem],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
