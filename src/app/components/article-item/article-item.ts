ba911e063de5f2f477d825a41b08a728fe36c4af
Júlia Romero <juliarore6@gmail.com>
2025-11-23 18:28:34 +0100

    feat: add first ArticleItem (Adidas Samba OG) with image

--- src/app/components/article-item/article-item.ts
+++ src/app/components/article-item/article-item.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Article } from '../../models/article.model';
 
 @Component({
   selector: 'app-article-item',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './article-item.html',
   styleUrl: './article-item.scss',
 })
 export class ArticleItem {
+
+  article: Article = {
+    name: 'Adidas Samba OG',
+    imageUrl: '/assets/images/adidas-samba-og.jpg',
+    price: 120,
+    isOnSale: true,
+    quantityInCart: 1
+  };
 
 }
