a696197ca7758011b0290a75de4d009accf56ceb
Júlia Romero <juliarore6@gmail.com>
2025-11-28 19:04:00 +0100

    feat: add ArticleItem component with conditional sale styling

--- src/app/components/article-item/article-item.ts
+++ src/app/components/article-item/article-item.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
 import { Article } from '../../models/article.model';
 
 @Component({
   selector: 'app-article-item',
-  imports: [NgClass],
+  imports: [],
   templateUrl: './article-item.html',
   styleUrl: './article-item.scss',
 })
