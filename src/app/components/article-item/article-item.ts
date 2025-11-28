0ccbadf9c309707a0e3b1fbb99663992026cdc6d
Júlia Romero <juliarore6@gmail.com>
2025-11-28 19:35:10 +0100

    feat:  show quantity controls depending on sale status and apply conditional price styling using ngClass

--- src/app/components/article-item/article-item.ts
+++ src/app/components/article-item/article-item.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { Article } from '../../models/article.model';
 
 @Component({
   selector: 'app-article-item',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './article-item.html',
   styleUrl: './article-item.scss',
 })
