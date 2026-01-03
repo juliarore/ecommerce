bd6a4b42e5eede81f7a19a168a42be17f684fd90
Júlia Romero <juliarore6@gmail.com>
2026-01-03 14:58:01 +0100

    feat: update routing and navbar for improved navigation using routerLink and routerLinkActive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ArticleList } from './components/article-list/article-list';
+import { ArticleDetail } from './components/article-detail/article-detail';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { ArticleNewReactive } from './components/article-new-reactive/article-new-reactive';
+import { ArticleNewTemplate } from './components/article-new-template/article-new-template';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'articles', component: ArticleList },
+  { path: 'article/:id', component: ArticleDetail },
+  { path: 'article-new-reactive', component: ArticleNewReactive },
+  { path: 'article-new-template', component: ArticleNewTemplate },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ArticleList } from './components/article-list/article-list';
 import { Navbar } from './components/navbar/navbar';
-import { ArticleNewTemplate } from './components/article-new-template/article-new-template';
-import { ArticleNewReactive } from "./components/article-new-reactive/article-new-reactive";
-import { Login } from './components/login/login';
-import { Register } from './components/register/register';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ArticleList, Navbar, ArticleNewTemplate, ArticleNewReactive, Login, Register],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('ecommerce');
-
-  currentView: string = 'articulos';
-
-  navbarChangeView(route: string) {
-    this.currentView = route;
-  }
 }
